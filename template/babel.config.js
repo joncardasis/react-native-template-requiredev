e32c9ea42db1cfc7094ffe63ff800e0c699bed1e
Jon Cardasis <joncardasis@gmail.com>
2022-01-21 15:02:03 -0500

    Add babel module resolver aliases

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          assets: './src/assets',
+          features: './src/features',
+          shared: './src/shared',
+          utils: './utils',
+        },
+      },
+    ],
+  ],
 };
